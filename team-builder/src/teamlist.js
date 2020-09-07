58403bd1352148d2c84a4ad94b9c616d58b60c8f
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-06 18:14:54 -0600

    added colors

--- team-builder/src/teamlist.js
+++ team-builder/src/teamlist.js
@@ -2,25 +2,30 @@ const TeamList = [
     {
         name: 'Heather',
         age: 35,
+        color: 'blue'
     },
 
     {
         name: 'Lonnie',
         age: 42,
+        color: 'green'
     },
 
     {
         name: 'Noa',
         age: 14,
+        color: 'purple'
     },
 
     {
         name: 'Raine',
         age: 13,
+        color: 'yellow'
     },
 
     {
         name: 'Saiya',
         age: 13,
+        color: 'black'
     }
 ]
