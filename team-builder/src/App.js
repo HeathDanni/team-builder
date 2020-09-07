efbbdce3f874f31bed5a9a101f1d136d50b0d540
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-07 15:43:20 -0600

    working on stretch, added editMember function

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -17,12 +17,18 @@ function App() {
 
     setTeam([...team, newMember]);
   };
+
+  // const editMember = () => {
+  //   const [memberToEdit, setMemberToEdit] = useState();
+  // }
  
   return (
 
     <div className="App">
       <h1>My Team</h1>
-          <TeamForm addNewMember={addNewMember}/>
+          <TeamForm addNewMember={addNewMember} 
+          // editMember={editMember}
+          />
           <TeamMember team={team} />
     </div>
   );
