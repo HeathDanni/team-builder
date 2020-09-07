bfc6e9c8d30d7680cd6feacc02dce291cc640b71
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-06 18:12:36 -0600

    added teamlist

---
+++ team-builder/src/teamlist.js
@@ -0,0 +1,26 @@
+const TeamList = [
+    {
+        name: 'Heather',
+        age: 35,
+    },
+
+    {
+        name: 'Lonnie',
+        age: 42,
+    },
+
+    {
+        name: 'Noa',
+        age: 14,
+    },
+
+    {
+        name: 'Raine',
+        age: 13,
+    },
+
+    {
+        name: 'Saiya',
+        age: 13,
+    }
+]
